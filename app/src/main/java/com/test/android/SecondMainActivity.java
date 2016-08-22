e5feb4ffea1cd3f127c25841dd8016e30483fdcc
manish rathi <manishrth06@gmail.com>
2016-08-22 14:30:52 +0530

    first commit

--- app/src/main/java/com/test/android/SecondMainActivity.java
+++ app/src/main/java/com/test/android/SecondMainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.test.com.mytestlb.MySampleTest;
 import android.view.View;
 import android.widget.Button;
 
